15c2b406f66dfc327faa8bc4d512307f59ab5569
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-10-05 15:41:17 +0100

    Added menu toggle with initial menu show/hide behaviour

---
+++ src/js/joose-navigation-full-screen.js
@@ -0,0 +1,45 @@
+// to es2015-ify
+
+// import joose-utils once is es2015 compatible
+
+;
+var joose = window.joose || {};
+joose.navigation = window.joose.navigation || {};
+joose.navigation.fullScreen = (function(joose) {
+
+    "use strict";
+
+    // shared configuration / variables
+    var config = {
+        menuShownClass: 'menu-shown'
+    };
+
+    // private functions
+    var toggleNav = function(parentElement, pageContentElement) {
+        if (joose.utils.hasClass(parentElement, config.menuShownClass)) {
+            joose.utils.removeClass(parentElement, config.menuShownClass);
+            pageContentElement.style.removeProperty('margin-top');
+        } else {
+            joose.utils.addClass(parentElement, config.menuShownClass);
+            pageContentElement.style.marginTop = window.innerHeight + 'px';
+        }
+    }
+
+    // initialise toggle links
+    var init = function (menuToggleLinks, parentElement, pageContentElement) {
+        menuToggleLinks.forEach(function(link) {
+            link.addEventListener('click', function(event) {
+                console.log(event)
+                event.preventDefault();
+                toggleNav(parentElement, pageContentElement);
+            });
+        });
+    }
+
+    // public functions
+    return {
+        toggleNav: toggleNav,
+        init: init
+    }
+
+})(joose);
